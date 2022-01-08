4c8bdd602b77972ee302cd59b2893500a4061746
w1lldone <wildan.arrahman@gmail.com>
2022-01-08 11:23:13 +0700

    Fix profile nav

--- resources/js/Components/ProfileNav.vue
+++ resources/js/Components/ProfileNav.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="nav nav-pills">
     <li class="nav-item">
-      <Link class="nav-link" :class="{ active: route().current('profile.index') }" aria-current="page" :href="route('profile.index')">Basic Info</Link>
+      <Link class="nav-link" :class="{ active: route().current('profile.index') }" aria-current="page" :href="route('profile.index')">Data diri</Link>
     </li>
     <li class="nav-item">
-      <Link class="nav-link" :class="{ active: route().current('profile.address.edit') }" aria-current="page" :href="route('profile.address.edit')">Address</Link>
+      <Link class="nav-link" :class="{ active: route().current('profile.address.edit') }" aria-current="page" :href="route('profile.address.edit')">Alamat</Link>
     </li>
   </ul>
 </template>
